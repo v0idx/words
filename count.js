2700923a014fc46ad91d94ab2848d36251b61674
Elliott Nash <56604081+v0idx@users.noreply.github.com>
2020-02-03 17:10:14 +0000

    Updated display.
    When over the word limit it will now display the words over.

--- count.js
+++ count.js
@@ -1,20 +1,27 @@
 function updateWordCounter(event) {
     var check = false;
+
     if (document.getElementById("maxWordCount").value != "") {
         check = !check;
         document.getElementById("wordCount").style = "color: white;";
     }
+
     const words = document.getElementById("textEntry").value.match(/\w+(-\w+)*/g);
     const maxWords = parseInt(document.getElementById("maxWordCount").value);
+
     if (!check) {
         maxWords == words.length;
     }
-    document.getElementById("wordCount").textContent = words.length;
+
     if ((words.length <= maxWords) && (check)) {
         document.getElementById("wordCount").style = "color: green;";
+        document.getElementById("wordCount").textContent = words.length;
     } else {
         if (check) {
             document.getElementById("wordCount").style = "color: red;";
+            document.getElementById("wordCount").textContent = words.length.toString().concat(" (", (maxWords - words.length).toString(), ")");
+        } else {
+            document.getElementById("wordCount").textContent = words.length;
         }
     }
 }
